1449ea9bae31eacf36d2db021ed2065a3aed6d52
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-10 10:27:31 -0700

    Creating FlatList to display the name of business

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ResultList = ({ title, result }) => {
 	return (
 		<View>
 			<Text style={stlyes.titleStyle}>{title}</Text>
+			<FlatList
+				horizontal
+				data={result}
+				keyExtractor={(current) => current.id}
+				renderItem={({ item }) => {
+					return <Text>{item.name}</Text>;
+				}}
+			/>
 			<Text>Amount: {result.length}</Text>
 		</View>
 	);
