7f85db3ae0c93752ec5dce9cea5293705c9f0c01
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-11 08:22:52 -0700

    Making the view scrollable vertically

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
 import ResultList from "../components/ResultList";
 import useSearch from "../hooks/useSearch";
@@ -9,17 +9,19 @@ const SearchScreen = () => {
 	const [error, result, searchAPI] = useSearch();
 
 	return (
-		<View>
+		<>
 			<SearchBar
 				onSearchTermChange={setSearchTerm}
 				searchTerm={searchTerm}
 				onEndEditing={() => searchAPI(searchTerm)}
 			/>
 			{error ? <Text>{error}</Text> : null}
-			<ResultList result={result["$"] || []} title="Cost Effective" />
-			<ResultList result={result["$$"] || []} title="Bit Pricier" />
-			<ResultList result={result["$$$"] || []} title="Big Spender" />
-		</View>
+			<ScrollView>
+				<ResultList result={result["$"] || []} title="Cost Effective" />
+				<ResultList result={result["$$"] || []} title="Bit Pricier" />
+				<ResultList result={result["$$$"] || []} title="Big Spender" />
+			</ScrollView>
+		</>
 	);
 };
 
